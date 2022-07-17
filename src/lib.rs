eaa0766eb1c97f707fdaded550461da994be59c7
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-07-17 18:52:33 -0300

    new "bare" variations of methods with unit

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ const BYTES: &str = "B";
 
 /// Human representation trait, already implemented for all Rust primitive number types.
 pub trait HumanRepr: sealed::Sealed + Sized {
-    /// Generate a beautiful human-readable count, supporting SI prefixes and others.
-    /// <br>If an even larger scale would be needed (possible with a [u128]), a "+" is used.
+    /// Generate a beautiful human-readable count, supporting SI/IEC prefixes to indicate multiples,
+    /// and your custom unit.
     ///
     /// ```
     /// use human_repr::HumanRepr;
@@ -21,9 +21,19 @@ pub trait HumanRepr: sealed::Sealed + Sized {
     /// ```
     fn human_count<T>(self, unit: T) -> HumanCount<T>;
 
-    /// Generate a beautiful human-readable count, using `"B"` as the unit.
+    /// Generate a beautiful human-readable count, supporting SI/IEC prefixes to indicate multiples,
+    /// and an empty unit.
     ///
-    /// See [Self::human_count()].
+    /// ```
+    /// use human_repr::HumanRepr;
+    /// assert_eq!("4.2M", 4221432u32.human_count_bare());
+    /// ```
+    fn human_count_bare(self) -> HumanCount<&'static str> {
+        self.human_count("")
+    }
+
+    /// Generate a beautiful human-readable count, supporting SI/IEC prefixes to indicate multiples,
+    /// and bytes `"B"` as the unit.
     ///
     /// ```
     /// use human_repr::HumanRepr;
@@ -33,8 +43,8 @@ pub trait HumanRepr: sealed::Sealed + Sized {
         self.human_count(BYTES)
     }
 
-    /// Generate a beautiful human-readable duration, supporting nanos (`ns`), millis (`ms`),
-    /// micros (`µs`), seconds (`s`), minutes (`M:SS`), and even hours (`H:MM:SS`).
+    /// Generate a beautiful human-readable duration, supporting SI prefixes nanos (`ns`), millis (`ms`),
+    /// micros (`µs`), and seconds (`s`), in addition to minutes (`M:SS`) and even hours (`H:MM:SS`).
     ///
     /// ```
     /// use human_repr::HumanRepr;
@@ -43,8 +53,7 @@ pub trait HumanRepr: sealed::Sealed + Sized {
     fn human_duration(self) -> HumanDuration;
 
     /// Generate a beautiful human-readable throughput, supporting per-day (`/d`), per-hour (`/h`),
-    /// per-month (`/m`), and per-sec (`/s`).
-    /// <br>When in `/s`, SI prefixes can appear, as in [Self::human_count()].
+    /// per-month (`/m`), and per-sec (`/s`), and your custom unit.
     ///
     /// ```
     /// use human_repr::HumanRepr;
@@ -52,9 +61,19 @@ pub trait HumanRepr: sealed::Sealed + Sized {
     /// ```
     fn human_throughput<T>(self, unit: T) -> HumanThroughput<T>;
 
-    /// Generate a beautiful human-readable throughput, using `"B"` as the unit.
+    /// Generate a beautiful human-readable throughput, supporting per-day (`/d`), per-hour (`/h`),
+    /// per-month (`/m`), and per-sec (`/s`), and an empty unit.
     ///
-    /// See [Self::human_throughput()].
+    /// ```
+    /// use human_repr::HumanRepr;
+    /// assert_eq!("1.2k/s", 1234.5.human_throughput_bare());
+    /// ```
+    fn human_throughput_bare(self) -> HumanThroughput<&'static str> {
+        self.human_throughput("")
+    }
+
+    /// Generate a beautiful human-readable throughput, supporting per-day (`/d`), per-hour (`/h`),
+    /// per-month (`/m`), and per-sec (`/s`), and bytes `"B"` as the unit.
     ///
     /// ```
     /// use human_repr::HumanRepr;
@@ -66,8 +85,8 @@ pub trait HumanRepr: sealed::Sealed + Sized {
 }
 
 pub trait HumanReprDuration: sealed::Sealed + Sized {
-    /// Generate a beautiful human-readable duration, supporting nanos (`ns`), millis (`ms`),
-    /// micros (`µs`), seconds (`s`), minutes (`M:SS`), and even hours (`H:MM:SS`).
+    /// Generate a beautiful human-readable duration, supporting SI prefixes nanos (`ns`), millis (`ms`),
+    /// micros (`µs`), and seconds (`s`), in addition to minutes (`M:SS`) and even hours (`H:MM:SS`).
     ///
     /// ```
     /// use human_repr::HumanReprDuration;
