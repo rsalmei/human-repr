37179ccb9ded8bf017c1f46de150321a34901eee
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-07-22 22:03:16 -0300

    new non-allocating PartialEq<&str>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod human_count;
 mod human_duration;
 mod human_throughput;
 
+use std::fmt;
+
 pub struct HumanCount<T>(f64, T);
 pub struct HumanDuration(f64);
 pub struct HumanThroughput<T>(f64, T);
@@ -140,4 +142,21 @@ fn rounded(val: f64, dec: usize) -> f64 {
         2 => (val * 100.).round() / 100.,
         _ => unreachable!(),
     }
+}
+
+struct DisplayCompare<'a, I>(&'a mut I);
+
+impl<I: Iterator<Item = u8>> fmt::Write for DisplayCompare<'_, I> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        match s.bytes().zip(self.0.by_ref()).all(|(x, y)| x == y) {
+            true => Ok(()),
+            false => Err(fmt::Error),
+        }
+    }
+}
+
+pub fn display_compare(str: &str, display: &impl fmt::Display) -> bool {
+    let mut it = str.bytes();
+    use fmt::Write;
+    write!(DisplayCompare(it.by_ref()), "{display}").map_or(false, |_| it.len() == 0)
 }
