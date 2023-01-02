51dc69e72156691729b8865332264ced8b09a8b8
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2023-01-02 14:37:01 -0300

    rounding tests

--- src/utils.rs
+++ src/utils.rs
@@ -28,3 +28,25 @@ pub fn display_compare(str: &str, display: &impl fmt::Display) -> bool {
     let mut it = str.bytes();
     write!(DisplayCompare(it.by_ref()), "{display}").map_or(false, |_| it.len() == 0)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rounding() {
+        assert_eq!(23456., rounded(23456.23456, 0));
+        assert_eq!(23456.2, rounded(23456.23456, 1));
+        assert_eq!(23456.23, rounded(23456.23456, 2));
+        assert_eq!(23456.235, rounded(23456.23456, 3));
+        assert_eq!(23456.2346, rounded(23456.23456, 4));
+        assert_eq!(23456.23456, rounded(23456.23456, 5));
+        assert_eq!(23460., rounded(23456.23456, -1));
+        assert_eq!(23500., rounded(23456.23456, -2));
+        assert_eq!(23000., rounded(23456.23456, -3));
+        assert_eq!(20000., rounded(23456.23456, -4));
+        assert_eq!(0., rounded(23456.23456, -5));
+        assert_eq!(23456.23456, rounded(23456.23456, i8::MAX));
+        assert_eq!(0., rounded(23456.23456, i8::MIN));
+    }
+}
