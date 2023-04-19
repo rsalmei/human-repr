261e5dd909cd9c83a3765a32974152872bccb6b9
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2023-04-19 02:15:50 -0300

    rename only

--- benches/counts.rs
+++ benches/counts.rs
@@ -2,16 +2,16 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use human_repr::HumanCount;
 use std::fmt::Write;
 
-pub struct Null;
+pub struct Void;
 
-impl Write for Null {
+impl Write for Void {
     fn write_str(&mut self, _s: &str) -> std::fmt::Result {
         Ok(())
     }
 }
 
 fn benchmark<T: HumanCount>(val: T) {
-    let _ = write!(Null, "{}", black_box(val).human_count_bytes());
+    let _ = write!(Void, "{}", black_box(val).human_count_bytes());
 }
 
 pub fn small(c: &mut Criterion) {
