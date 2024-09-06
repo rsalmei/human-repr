31e15b306a5345a72898bb91d603c8a8103c1ee2
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2024-09-06 17:22:51 -0300

    final Repr sybsystem

--- src/human_count/repr.rs
+++ src/human_count/repr.rs
@@ -1,84 +1,103 @@
-use crate::metric_prefix::System;
-use crate::repr_base::{Precision, Separator};
-use crate::utils;
+use crate::{utils, Precision, Split, System};
 use std::borrow::Cow;
-use std::fmt::{self, Display, Formatter};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::sync::RwLock;
 
-/// Representation options for Human Count.
-#[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd)]
+/// The HumanCount representation options.
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
 pub struct HumanCountRepr {
     pub unit: &'static str,
     pub sys: Option<System>,
     pub prec: Option<Precision>,
-    pub sep: Option<Separator>,
+    pub split: Option<Split>,
 }
 
-/// This is used in the Debug impl of the [`super::HumanCountData`].
+/// This is used in the Debug impl of the [`HumanCountData`].
 impl Display for HumanCountRepr {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "({}", self.unit)?;
+        let sep = |f: &mut Formatter<'_>, previous| {
+            if previous {
+                write!(f, "+")?;
+            }
+            Ok::<_, fmt::Error>(())
+        };
+        let mut previous = false;
+        if !self.unit.is_empty() {
+            write!(f, "{:?}", self.unit)?;
+            previous = true;
+        }
         if let Some(sys) = self.sys {
-            write!(f, "_{sys:?}")?;
+            sep(f, previous)?;
+            write!(f, "{sys:?}")?;
+            previous = true;
         }
         if let Some(prec) = self.prec {
-            write!(f, ":{prec}")?;
+            sep(f, previous)?;
+            write!(f, "{prec:?}")?;
+            previous = true;
         }
-        if let Some(sep) = self.sep {
-            write!(f, "-{sep:?}")?;
+        if let Some(split) = self.split {
+            sep(f, previous)?;
+            write!(f, "{split:?}")?;
+            previous = true;
         }
-        write!(f, ")")
+        if !previous {
+            write!(f, "()")?;
+        }
+        Ok(())
     }
 }
 
-trait UpdateHumanCountRepr {
-    fn update_repr(self, repr: &mut HumanCountRepr);
+trait HumanCountReprParts {
+    fn assign_into(self, repr: &mut HumanCountRepr);
 }
 
-impl UpdateHumanCountRepr for &'static str {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for &'static str {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.unit = self;
     }
 }
 
-impl UpdateHumanCountRepr for String {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for String {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.unit = utils::intern(self);
     }
 }
 
-impl UpdateHumanCountRepr for Cow<'_, str> {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for Cow<'_, str> {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.unit = utils::intern(self);
     }
 }
 
-impl UpdateHumanCountRepr for System {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for System {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.sys = Some(self);
     }
 }
 
-impl UpdateHumanCountRepr for u8 {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for u8 {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.prec = Some(Precision::from(self));
     }
 }
 
-impl UpdateHumanCountRepr for Precision {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
+impl HumanCountReprParts for Precision {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
         repr.prec = Some(self);
     }
 }
 
-impl UpdateHumanCountRepr for bool {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
-        repr.sep = Some(Separator::from(self));
+impl HumanCountReprParts for bool {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
+        repr.split = Some(Split::from(self));
     }
 }
 
-impl UpdateHumanCountRepr for Separator {
-    fn update_repr(self, repr: &mut HumanCountRepr) {
-        repr.sep = Some(self);
+impl HumanCountReprParts for Split {
+    fn assign_into(self, repr: &mut HumanCountRepr) {
+        repr.split = Some(self);
     }
 }
 
@@ -86,81 +105,146 @@ pub trait IntoHumanCountRepr {
     fn into_repr(self) -> HumanCountRepr;
 }
 
-impl<T: UpdateHumanCountRepr> IntoHumanCountRepr for T {
+impl<T: HumanCountReprParts> IntoHumanCountRepr for T {
     fn into_repr(self) -> HumanCountRepr {
         (self,).into_repr()
     }
 }
 
+impl IntoHumanCountRepr for () {
+    fn into_repr(self) -> HumanCountRepr {
+        HumanCountRepr::default()
+    }
+}
+
 impl<T1> IntoHumanCountRepr for (T1,)
 where
-    T1: UpdateHumanCountRepr,
+    T1: HumanCountReprParts,
 {
     fn into_repr(self) -> HumanCountRepr {
         let mut repr = HumanCountRepr::default();
-        self.0.update_repr(&mut repr);
+        self.0.assign_into(&mut repr);
         repr
     }
 }
 
 impl<T1, T2> IntoHumanCountRepr for (T1, T2)
 where
-    T1: UpdateHumanCountRepr,
-    T2: UpdateHumanCountRepr,
+    T1: HumanCountReprParts,
+    T2: HumanCountReprParts,
 {
     fn into_repr(self) -> HumanCountRepr {
         let mut repr = HumanCountRepr::default();
-        self.0.update_repr(&mut repr);
-        self.1.update_repr(&mut repr);
+        self.0.assign_into(&mut repr);
+        self.1.assign_into(&mut repr);
         repr
     }
 }
 
 impl<T1, T2, T3> IntoHumanCountRepr for (T1, T2, T3)
 where
-    T1: UpdateHumanCountRepr,
-    T2: UpdateHumanCountRepr,
-    T3: UpdateHumanCountRepr,
+    T1: HumanCountReprParts,
+    T2: HumanCountReprParts,
+    T3: HumanCountReprParts,
 {
     fn into_repr(self) -> HumanCountRepr {
         let mut repr = HumanCountRepr::default();
-        self.0.update_repr(&mut repr);
-        self.1.update_repr(&mut repr);
-        self.2.update_repr(&mut repr);
+        self.0.assign_into(&mut repr);
+        self.1.assign_into(&mut repr);
+        self.2.assign_into(&mut repr);
         repr
     }
 }
 
 impl<T1, T2, T3, T4> IntoHumanCountRepr for (T1, T2, T3, T4)
 where
-    T1: UpdateHumanCountRepr,
-    T2: UpdateHumanCountRepr,
-    T3: UpdateHumanCountRepr,
-    T4: UpdateHumanCountRepr,
+    T1: HumanCountReprParts,
+    T2: HumanCountReprParts,
+    T3: HumanCountReprParts,
+    T4: HumanCountReprParts,
 {
     fn into_repr(self) -> HumanCountRepr {
         let mut repr = HumanCountRepr::default();
-        self.0.update_repr(&mut repr);
-        self.1.update_repr(&mut repr);
-        self.2.update_repr(&mut repr);
-        self.3.update_repr(&mut repr);
+        self.0.assign_into(&mut repr);
+        self.1.assign_into(&mut repr);
+        self.2.assign_into(&mut repr);
+        self.3.assign_into(&mut repr);
         repr
     }
 }
 
+static COUNT_SYS: RwLock<System> = RwLock::new(System::SI);
+static COUNT_PREC: RwLock<Precision> = RwLock::new(Precision::Auto);
+static COUNT_SPLIT: RwLock<Split> = RwLock::new(Split::Join);
+
 impl HumanCountRepr {
-    pub fn new<'a, P: Into<Precision>, S: Into<Separator>>(
+    /// Create a new default HumanCount representation. Can be used as a builder.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Set the unit of this HumanCount representation.
+    pub fn unit(mut self, unit: impl Into<Cow<'static, str>>) -> Self {
+        self.unit = utils::intern(unit);
+        self
+    }
+
+    /// Set the system of this HumanCount representation.
+    pub fn system(mut self, sys: impl Into<Option<System>>) -> Self {
+        self.sys = sys.into();
+        self
+    }
+
+    /// Set the precision of this HumanCount representation.
+    pub fn precision<P: Into<Precision>>(mut self, prec: impl Into<Option<P>>) -> Self {
+        self.prec = prec.into().map(Into::into);
+        self
+    }
+
+    /// Set the Split of this HumanCount representation.
+    pub fn split<S: Into<Split>>(mut self, split: impl Into<Option<S>>) -> Self {
+        self.split = split.into().map(Into::into);
+        self
+    }
+
+    /// Create a new HumanCount representation with the given parts.
+    pub fn with_parts<'a>(
         unit: impl Into<Cow<'a, str>>,
         sys: impl Into<Option<System>>,
-        prec: impl Into<Option<P>>,
-        sep: impl Into<Option<S>>,
+        prec: impl Into<Option<Precision>>,
+        split: impl Into<Option<Split>>,
     ) -> Self {
         Self {
-            unit: utils::intern(unit.into()),
+            unit: utils::intern(unit),
             sys: sys.into(),
             prec: prec.into().map(Into::into),
-            sep: sep.into().map(Into::into),
+            split: split.into().map(Into::into),
         }
+    }
+
+    /// Set the default System used for HumanCount.
+    pub fn default_system(sys: System) {
+        *COUNT_SYS.write().unwrap() = sys
+    }
+
+    /// Set the default precision used for HumanCount.
+    pub fn default_precision(prec: Precision) {
+        *COUNT_PREC.write().unwrap() = prec
+    }
+
+    /// Set the default Split used for HumanCount.
+    pub fn default_split(split: Split) {
+        *COUNT_SPLIT.write().unwrap() = split
+    }
+
+    /// Resolve the options from this HumanCount representation with the global settings.
+    pub(super) fn resolve(&self) -> (&'static str, System, Precision, Split) {
+        (
+            self.unit,
+            self.sys.unwrap_or_else(|| *COUNT_SYS.read().unwrap()),
+            self.prec.unwrap_or_else(|| *COUNT_PREC.read().unwrap()),
+            self.split.unwrap_or_else(|| *COUNT_SPLIT.read().unwrap()),
+        )
     }
 }
 
@@ -169,20 +253,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn builder() {
+        let repr = HumanCountRepr {
+            unit: "X",
+            sys: Some(System::IEC),
+            prec: Some(Precision::Fixed(1)),
+            split: Some(Split::Prefix),
+        };
+        assert_eq!(
+            repr,
+            HumanCountRepr::new()
+                .unit("X")
+                .system(System::IEC)
+                .precision(1)
+                .split(true)
+        );
+        assert_eq!(
+            repr,
+            HumanCountRepr::with_parts("X", System::IEC, Precision::Fixed(1), Split::Prefix)
+        );
+    }
+
+    #[test]
     fn magic_into_repr() {
         // this will generate exactly 446 test cases, with all possible permutations
         // of the parameters, including directly T and tuples with 1 to 4 elements.
         // asked on the Rust users forum: https://users.rust-lang.org/t/macro-to-generate-combinations-for-test-cases/116416
         macro_rules! base {
-            ($repr:expr => $unit:expr, $sys:expr, $prec:expr, $sep:expr) => {
+            ($repr:expr => $unit:expr, $sys:expr, $prec:expr, $split:expr) => {
                 assert_eq!(
-                    // HumanCountRepr::new($unit, $sys, $prec, $sep),
-                    HumanCountRepr {
-                        unit: $unit.into(),
-                        sys: $sys.into(),
-                        prec: $prec.into(),
-                        sep: $sep.into(),
-                    },
+                    HumanCountRepr::with_parts($unit, $sys, $prec, $split),
                     $repr.into_repr()
                 );
             };
@@ -229,9 +329,9 @@ mod tests {
                 case!(@[$($processed,)* $a                 ,] ($($($more)*)?) => $($p),*);
                 case!(@[$($processed,)* Precision::from($a),] ($($($more)*)?) => $($p),*);
             };
-            (@[$($processed:expr,)*] (@sep $a:expr $(, $($more:tt)*)?) => $($p:expr),*) => {
+            (@[$($processed:expr,)*] (@split $a:expr $(, $($more:tt)*)?) => $($p:expr),*) => {
                 case!(@[$($processed,)* $a                 ,] ($($($more)*)?) => $($p),*);
-                case!(@[$($processed,)* Separator::from($a),] ($($($more)*)?) => $($p),*);
+                case!(@[$($processed,)* Split::from($a),] ($($($more)*)?) => $($p),*);
             };
             (@[$($processed:expr,)*] ($a:expr $(, $($more:tt)*)?) => $($p:expr),*) => {
                 case!(@[$($processed,)* $a              ,] ($($($more)*)?) => $($p),*);
@@ -248,25 +348,26 @@ mod tests {
         }
 
         // --- one argument.
+        case!(() => "", None, None, None);
         case!(@unit "U" => "U", None, None, None);
         case!(System::SI2 => "", System::SI2, None, None);
         case!(@prec 5 => "", None, Precision::Fixed(5), None);
-        case!(@sep false => "", None, None, Separator::No);
+        case!(@split false => "", None, None, Split::Join);
 
         // --- two arguments.
         case!((@unit "U", System::SI2) => "U", System::SI2, None, None);
         case!((@unit "U", @prec 5) => "U", None, Precision::Fixed(5), None);
-        case!((@unit "U", @sep false) => "U", None, None, Separator::No);
+        case!((@unit "U", @split false) => "U", None, None, Split::Join);
         case!((System::SI2, @prec 5) => "", System::SI2, Precision::Fixed(5), None);
-        case!((System::SI2, @sep false) => "", System::SI2, None, Separator::No);
-        case!((@prec 5, @sep false) => "", None, Precision::Fixed(5), Separator::No);
+        case!((System::SI2, @split false) => "", System::SI2, None, Split::Join);
+        case!((@prec 5, @split false) => "", None, Precision::Fixed(5), Split::Join);
 
         // --- three arguments.
         case!((@unit "U", System::SI2, @prec 5) => "U", System::SI2, Precision::Fixed(5), None);
-        case!((@unit "U", System::SI2, @sep false) => "U", System::SI2, None, Separator::No);
-        case!((System::SI2, @prec 5, @sep false) => "", System::SI2, Precision::Fixed(5), Separator::No);
+        case!((@unit "U", System::SI2, @split false) => "U", System::SI2, None, Split::Join);
+        case!((System::SI2, @prec 5, @split false) => "", System::SI2, Precision::Fixed(5), Split::Join);
 
         // --- four arguments.
-        case!((@unit "U", System::SI2, @prec 5, @sep false) => "U", System::SI2, Precision::Fixed(5), Separator::No);
+        case!((@unit "U", System::SI2, @prec 5, @split false) => "U", System::SI2, Precision::Fixed(5), Split::Join);
     }
 }
